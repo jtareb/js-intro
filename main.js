dc235d47c8aa2e2358c7337c8ab4e8d2b515f32a
John Albritton <jtareb1@att.net>
2015-01-13 20:43:41 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,48 @@
+var name;
+// Q: What does name equal right now?
+A: undefined
+
+
+name = "Tim";
+// Q: What is `name` set to right now?
+Tim
+
+
+name = "Bill";
+// Q: What is `name` set to right now?
+Bill
+
+
+var greeting = "Hello, how are you?";
+// Q: What is `greeting` set to?
+Hello, how are you?
+
+
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+// Q: What is `math` set to?
+40
+
+
+math = high - "5";
+// Q: What is `math` set to?
+-5
+
+
+// Create a variable to calculate my age
+// The answer should read "Tim is 31 years old"
+var born = 1983;
+var today = 2015;
+2015-1983
+32
+"Tim is 32 years old"
+
+
+// Store some information following in variables.
+var yourname;
+var myname;
+
+var statement = yourname + " is taking a class at The Iron Yard, and my instructor is " + myname;
